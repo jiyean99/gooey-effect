63fbf78c60912578c24698de58bdae8f5a630286
gui.ljy <gui.ljy@zempublic.co.kr>
2023-06-09 15:29:30 +0900

    #캔버스 파티클 다루기 예제
    [설명] dat-gui 라이브러리 활용 추

--- js/script.js
+++ js/script.js
@@ -18,6 +18,36 @@ canvas.width = canvasWidth * dpr
 canvas.height = canvasHeight * dpr
 ctx.scale(dpr, dpr)
 
+const feGaussianBlur = document.querySelector('feGaussianBlur')
+const feColorMatrix = document.querySelector('feColorMatrix')
+
+const controls = new function(){
+  this.blurValue = 40
+  this.alphaChannel = 100
+  this.alphaOffeset = -23
+  this.acc = 1.03
+}
+
+let gui = new dat.GUI()
+
+const f1 = gui.addFolder('Gooey Effect')
+const f2 = gui.addFolder('Particle Property')
+f1.open()
+f2.open()
+
+f1.add(controls, 'blurValue', 0, 100).onChange(value => {
+  feGaussianBlur.setAttribute('stdDeviation', value)
+})
+f1.add(controls, 'alphaChannel', 1, 200).onChange(value => {
+  feColorMatrix.setAttribute('values', '1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${value} ${controls.alphaOffeset}')
+})
+f1.add(controls, 'alphaOffeset', -40, 40).onChange(value => {
+  feColorMatrix.setAttribute('values', '1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${controls.alphaChannel} ${value}')
+})
+f2.add(controls, 'acc', 1, 1.5, 0.01).onChange(value => {
+  particles.forEach(particle => particle.acc = value)
+})
+
 // 사각형 그리기 : fillRect
 // ctx.fillRect(10, 10, 50, 50)
 
